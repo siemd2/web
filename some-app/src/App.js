f98db53e655ba601511d9967d5a0273485d2f3da
Siem Debesay <siem3500@gmail.com>
2023-05-17 22:34:27 -0600

    Added various components and nested function in App

--- some-app/src/App.js
+++ some-app/src/App.js
@@ -1,15 +1,16 @@
-
+import Todo from './components/Todo.js'
+import Modal from './components/Modal.js'
+import Backdrop from './components/Backdrop.js'
 
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <div className = "card">
-        <h2>TITLE</h2>
-        <div className = "actions">
-          <button className = "btn">Delete</button>
-        </div>
-      </div>
+      <Todo text="learn react.js"/>
+      <Todo text="learn next.js"/>
+      <Todo text="learn node.js"/>
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
--- some-app/src/components/Todo.js
+++ some-app/src/components/Todo.js
@@ -1,6 +1,15 @@
-function Todo() 
-{
-    
+function Todo(props) {
+    function deleteHandler() {  // standard convention to use nested functions to be used as props
+        
+    }
+    return (
+        <div className = "card">
+            <h2>{props.text}</h2>
+            <div className = "actions">
+                <button className = "btn" onClick={deleteHandler}>Delete</button>
+            </div>
+        </div>
+    );
 }
 
 export default Todo;
